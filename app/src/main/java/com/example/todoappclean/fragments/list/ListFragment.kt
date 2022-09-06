a643f5a45a185e40a389352e2e40ca04d3dbea9c
Greg L <lupasco.grigori@gmail.com>
2022-09-06 13:46:29 +0300

    design UpdateFragment layout + custom input

--- app/src/main/java/com/example/todoappclean/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todoappclean/fragments/list/ListFragment.kt
@@ -5,21 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.navigation.fragment.findNavController
 import com.example.todoappclean.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListFragment : Fragment() {
 
+    private lateinit var listLayout: ConstraintLayout
+    private lateinit var fab_add: FloatingActionButton
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view =  inflater.inflate(R.layout.fragment_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
 
-        view.findViewById<FloatingActionButton>(R.id.fab_add).setOnClickListener{
+        fab_add = view.findViewById(R.id.fab_add)
+        listLayout = view.findViewById(R.id.listLayout)
+
+
+        fab_add.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        listLayout.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
 
         return view
