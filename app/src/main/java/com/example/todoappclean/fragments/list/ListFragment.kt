89114b475c848ecb11d9e16862bf300c0260d045
Greg L <lupasco.grigori@gmail.com>
2022-09-08 17:32:40 +0300

    display data in RecyclerView + bug fixes

--- app/src/main/java/com/example/todoappclean/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todoappclean/fragments/list/ListFragment.kt
@@ -2,19 +2,24 @@ package com.example.todoappclean.fragments.list
 
 import android.os.Bundle
 import android.view.*
-import android.widget.SearchView
-import androidx.core.view.MenuHost
 import androidx.fragment.app.Fragment
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.MenuProvider
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.todoappclean.R
+import com.example.todoappclean.data.viewmodel.ToDoViewModel
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ListFragment : Fragment() {
 
     private lateinit var listLayout: ConstraintLayout
-    private lateinit var fab_add: FloatingActionButton
+    private lateinit var fabAdd: FloatingActionButton
+
+    private val mToDoViewModel: ToDoViewModel by viewModels()
+    private val adapter: ListAdapter by lazy { ListAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,11 +28,18 @@ class ListFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_list, container, false)
 
-        fab_add = view.findViewById(R.id.fab_add)
+        fabAdd = view.findViewById(R.id.fab_add)
         listLayout = view.findViewById(R.id.listLayout)
 
+        val rvFragmentList = view.findViewById<RecyclerView>(R.id.rvFragmentList)
+        rvFragmentList.adapter = adapter
+        rvFragmentList.layoutManager = LinearLayoutManager(requireActivity())
 
-        fab_add.setOnClickListener {
+        mToDoViewModel.getAllData.observe(viewLifecycleOwner, Observer { data ->
+            adapter.setData(data)
+        })
+
+        fabAdd.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
 
