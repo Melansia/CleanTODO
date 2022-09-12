39f2c00b5d5371c8d2b2e2426779c7d01694ee57
Greg L <lupasco.grigori@gmail.com>
2022-09-12 13:28:06 +0300

    implemented Data Biding for UpdateFragment

--- app/src/main/java/com/example/todoappclean/fragments/BidingAdapters.kt
+++ app/src/main/java/com/example/todoappclean/fragments/BidingAdapters.kt
@@ -1,10 +1,12 @@
 package com.example.todoappclean.fragments
 
 import android.view.View
+import android.widget.Spinner
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.MutableLiveData
 import androidx.navigation.findNavController
 import com.example.todoappclean.R
+import com.example.todoappclean.data.models.Priority
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class BidingAdapters {
@@ -24,11 +26,26 @@ class BidingAdapters {
         @BindingAdapter("android:emptyDatabase")
         @JvmStatic
         fun emptyDatabase(view: View, emptyDatabase: MutableLiveData<Boolean>) {
-            when(emptyDatabase.value) {
+            when (emptyDatabase.value) {
                 true -> view.visibility = View.VISIBLE
                 else -> view.visibility = View.INVISIBLE
             }
         }
 
+        @BindingAdapter("android:parsePriorityToInt")
+        @JvmStatic
+        fun parsePriorityToInt(view: Spinner, priority: Priority) {
+            when (priority) {
+                Priority.HIGH -> {
+                    view.setSelection(0)
+                }
+                Priority.MEDIUM -> {
+                    view.setSelection(1)
+                }
+                Priority.LOW -> {
+                    view.setSelection(2)
+                }
+            }
+        }
     }
 }
